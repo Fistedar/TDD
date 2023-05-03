d1b6cd628bccb3323ca8cb7cf08379885ebcc251
Artem <aidar741@gmail.com>
2023-05-03 22:01:40 +0300

    Stub and tests for findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -2,9 +2,14 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class PhoneBook {
-    Map<String,String> phoneBook = new TreeMap<>();
-    public int add(String name,String num){
-        phoneBook.put(name,num);
+    Map<String, String> phoneBook = new TreeMap<>();
+
+    public int add(String name, String num) {
+        phoneBook.put(name, num);
         return phoneBook.size();
+    }
+
+    public String findByNumber(String num) {
+        return null;
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -4,15 +4,27 @@ import static org.junit.Assert.assertEquals;
 
 public class PhoneBookTest {
     @Test
-    public void testAdd(){
-      PhoneBook phoneBook = new PhoneBook();
-        String testName = "Test";
-        String testNumber = "1234567";
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Test";
+        String num = "1234567";
 
         int expected = 1;
 
-        int actual = phoneBook.add(testName, testNumber);
+        int actual = phoneBook.add(name, num);
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testFindByNumber(){
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Test";
+        String num = "1234567";
+
+        String expected = "Test";
+        String actual = phoneBook.findByNumber(num);
+
+        assertEquals(expected,actual);
     }
+}
