b5366535153f036d2a4a77c77b95ea10a3777563
Artem <aidar741@gmail.com>
2023-05-03 21:51:45 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,5 @@
 public class PhoneBook {
+    public int add(String name,String num){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,18 @@
-public class PhoneBookTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class PhoneBookTest {
+    @Test
+    public void testAdd(){
+      PhoneBook phoneBook = new PhoneBook();
+        String testName = "Test";
+        String testNumber = "1234567";
+
+        int expected = 1;
+
+        int actual = phoneBook.add(testName, testNumber);
+
+        assertEquals(expected, actual);
+    }
     }
